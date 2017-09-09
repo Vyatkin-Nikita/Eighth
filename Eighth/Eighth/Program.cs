d9488a55bfa569fd60fdbd6a62216d600d8963e3
Вяткин Никита Алексеевич <navyatkin@edu.hse.ru>
2017-09-10 01:37:17 +0500

    Готовый вариант программы. Версия 1.0

--- Eighth/Eighth/Program.cs
+++ Eighth/Eighth/Program.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 
 namespace Eighth
 {
+    /* 
+       Программа находит все мосты в графе, заданном матрицей смежности.
+       Графы создаются генератором тестов.
+    */
     class Program
     {
-        static bool[,] MyGraph;
+        const int n = 5;//Число вершин в графе
+        static bool[,] MyGraph;//Граф в виде матрицы смежности
+
+        static int timer, count = 0;//count - количество мостов в графе, timer - перменная, для обхода графа в глубину
+        static int[] tin = new int[n];
+        static int[] fup = new int[n];
+        static bool[] used = new bool[n];
+
         static bool[,] GraphGenerator(int size)
         {
             Random rnd = new Random();
@@ -24,12 +35,12 @@ namespace Eighth
                         if (t == 0) { Graph[i, j] = false; }
                         else { Graph[i, j] = true; }
                     }
-                    if (i > j) { Graph[i, j] = Graph[j,i]; }
+                    if (i > j) { Graph[i, j] = Graph[j, i]; }
                 }
             }
             return Graph;
-        }
-        static void ShowGraph (bool [,] grh)
+        }//Генератор матриц смежности (он же генератор тестов)
+        static void ShowGraph(bool[,] grh)
         {
             string str = "  ";
             for (int d = 1; d <= grh.GetLength(1); d++)
@@ -38,10 +49,16 @@ namespace Eighth
                 str += d;
                 str += " ";
             }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine(str);
+            Console.ResetColor();
 
             for (int i = 0; i < grh.GetLength(0); i++)
             {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(i + 1 + " ");
+                Console.ResetColor();
                 for (int j = 0; j < grh.GetLength(1); j++)
                 {
                     if (grh[i, j] == true) Console.Write(1 + " ");
@@ -49,11 +66,46 @@ namespace Eighth
                 }
                 Console.WriteLine();
             }
+        }//Показать граф в виде матрицы смежности
+        static void DFS(int v, int p = -1)
+        {
+            used[v] = true;
+            tin[v] = fup[v] = timer++;
+            for (int i = 0; i < n; ++i)
+            {
+                if (MyGraph[v, i] == true)
+                {
+                    int to = i;
+                    if (to == p) continue;
+                    if (used[to]) fup[v] = Math.Min(fup[v], tin[to]);
+                    else
+                    {
+                        DFS(to, v);
+                        fup[v] = Math.Min(fup[v], fup[to]);
+
+                        if (fup[to] > tin[v]) { v++; to++; Console.WriteLine("Мост: (" + v + " , " + to + ")"); v--; to--; count++; }
+                    }
+                }
+            }
+        }//Обход графа в глубину и поиск мостов
+        static void FindBridges() //Поиск мостов
+        {
+            timer = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                used[i] = false;
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                if (!used[i]) DFS(i);
+            }
+            if (count == 0) Console.WriteLine("Мостов нет");
         }
         static void Main(string[] args)
         {
-            MyGraph = GraphGenerator(4);
+            MyGraph = GraphGenerator(n);
             ShowGraph(MyGraph);
+            FindBridges();
             Console.ReadLine();
         }
     }
